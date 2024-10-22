12a457b46ae2627dbd60df635d68683b98983504
Dokesh Kumar Gadtia <110685614+thedk201003@users.noreply.github.com>
2024-10-23 01:15:35 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Check if routes directory exists
+try {
+    const routes = readdirSync('./routes');
+    routes.forEach((route) => {
+        app.use('/api', require('./routes/' + route));
+    });
+} catch (error) {
+    console.error('Error loading routes:', error);
+    process.exit(1);
+}
+
+// Start server
+const server = () => {
+    db().then(() => {
+        app.listen(PORT, () => {
+            console.log('Server is running on port:', PORT);
+        });
+    }).catch((error) => {
+        console.error('DB Connection Error:', error);
+        process.exit(1); // Exit process with failure
+    });
+};
+
+server();
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,jpg,png}', './index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
